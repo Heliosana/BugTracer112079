4360f9502bce86eefcea67c627ff8c213cbb515a
Heliosana <krummnacker.m@gmail.com>
2013-06-05 17:54:31 +0200

    initial commit BgTracer

---
+++ BugTracer/src/Bugtracer/BugTracer.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package Bugtracer;
+
+/**
+ * @author Heliosana
+ *
+ */
+public class BugTracer {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
