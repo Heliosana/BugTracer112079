3a765673bdb35a7baef3fa56114f0ff22c1ed138
Heliosana <krummnacker.m@gmail.com>
2013-06-10 22:00:25 +0200

    gui extentions added & control panel class added to confirm the sql querys

---
+++ BugTracer/src/Bugtracer/ControlPanel.java
@@ -0,0 +1,15 @@
+package Bugtracer;
+
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+
+public class ControlPanel extends JPanel {
+
+	private Gui gui;
+	private JTabbedPane tPane;
+
+	public ControlPanel(Gui gui) {
+		this.gui = gui;
+	}
+
+}
--- BugTracer/src/Bugtracer/Gui.java
+++ BugTracer/src/Bugtracer/Gui.java
@@ -2,27 +2,25 @@ package Bugtracer;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.TextArea;
-import java.awt.Window;
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
-import javax.swing.JFrame;
-import javax.swing.JPasswordField;
-import javax.swing.JTextArea;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
-import javax.swing.JButton;
-import javax.swing.JToggleButton;
-import javax.swing.SwingConstants;
-import javax.swing.BoxLayout;
-import javax.swing.JLabel;
-import java.awt.FlowLayout;
-import javax.swing.JSeparator;
-import java.awt.CardLayout;
 import java.sql.Statement;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
 import javax.swing.JTabbedPane;
-import javax.swing.JTextPane;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import java.awt.Font;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.LineBorder;
 
 public class Gui implements ActionListener {
 
@@ -36,6 +34,11 @@ public class Gui implements ActionListener {
 	private JTextField serverNameTextfield;
 	private JTextField serverIPTextfield;
 	private JLabel statepane;
+	private JTabbedPane tableTPanel;
+	private JPanel mainTPanel;
+	private JPanel testPanel;
+	private JButton btnAdd;
+	private JButton btnTest;
 
 	public Gui(BugTracer bugTracer) {
 		this.bugTracer = bugTracer;
@@ -48,12 +51,27 @@ public class Gui implements ActionListener {
 	private void initialize() {
 		frame = new JFrame();
 		frame.setTitle("Bugtracer");
-		frame.setBounds(0, 0, 1127, 822);
+		frame.setBounds(0, 0, 883, 710);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[] { 1101, 10, 0 };
+		gridBagLayout.rowHeights = new int[] { 33, 737, 14, 0 };
+		gridBagLayout.columnWeights = new double[] { 1.0, 0.0, Double.MIN_VALUE };
+		gridBagLayout.rowWeights = new double[] { 0.0, 1.0, 0.0,
+				Double.MIN_VALUE };
+		frame.getContentPane().setLayout(gridBagLayout);
 
 		loginPanel = new JPanel();
-		frame.getContentPane().add(loginPanel, BorderLayout.NORTH);
-		loginPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		loginPanel.setBorder(new LineBorder(new Color(0, 0, 0)));
+		GridBagConstraints gbc_loginPanel = new GridBagConstraints();
+		gbc_loginPanel.gridwidth = 2;
+		gbc_loginPanel.anchor = GridBagConstraints.NORTH;
+		gbc_loginPanel.fill = GridBagConstraints.HORIZONTAL;
+		gbc_loginPanel.insets = new Insets(0, 0, 5, 0);
+		gbc_loginPanel.gridx = 0;
+		gbc_loginPanel.gridy = 0;
+		frame.getContentPane().add(loginPanel, gbc_loginPanel);
+		loginPanel.setLayout(new FlowLayout(FlowLayout.RIGHT, 5, 5));
 
 		JLabel lblServerip = new JLabel("Server-IP:");
 		loginPanel.add(lblServerip);
@@ -98,19 +116,63 @@ public class Gui implements ActionListener {
 		btnLogout.addActionListener(this);
 		loginPanel.add(btnLogout);
 
-		JPanel controlPanel = new JPanel();
-		frame.getContentPane().add(controlPanel, BorderLayout.EAST);
+		tableTPanel = new JTabbedPane(JTabbedPane.TOP);
+		GridBagConstraints gbc_tableTabbedPanel = new GridBagConstraints();
+		gbc_tableTabbedPanel.fill = GridBagConstraints.BOTH;
+		gbc_tableTabbedPanel.insets = new Insets(0, 0, 5, 5);
+		gbc_tableTabbedPanel.gridx = 0;
+		gbc_tableTabbedPanel.gridy = 1;
+		frame.getContentPane().add(tableTPanel, gbc_tableTabbedPanel);
 
-		JTabbedPane tableTabbedPanel = new JTabbedPane(JTabbedPane.TOP);
-		frame.getContentPane().add(tableTabbedPanel, BorderLayout.CENTER);
+		mainTPanel = new JPanel();
+		tableTPanel.addTab("New tab", null, mainTPanel, null);
+
+		testPanel = new JPanel();
+		tableTPanel.addTab("New tab", null, testPanel, null);
+
+		JPanel controlPanel = new ControlPanel(this);
+		controlPanel.setBorder(new LineBorder(new Color(0, 0, 0)));
+		GridBagConstraints gbc_controlPanel = new GridBagConstraints();
+		gbc_controlPanel.gridheight = 2;
+		gbc_controlPanel.anchor = GridBagConstraints.WEST;
+		gbc_controlPanel.fill = GridBagConstraints.VERTICAL;
+		gbc_controlPanel.insets = new Insets(0, 0, 5, 0);
+		gbc_controlPanel.gridx = 1;
+		gbc_controlPanel.gridy = 1;
+		frame.getContentPane().add(controlPanel, gbc_controlPanel);
+		GridBagLayout gbl_controlPanel = new GridBagLayout();
+		gbl_controlPanel.columnWidths = new int[] { 60, 0 };
+		gbl_controlPanel.rowHeights = new int[] { 23, 0, 0 };
+		gbl_controlPanel.columnWeights = new double[] { 0.0, Double.MIN_VALUE };
+		gbl_controlPanel.rowWeights = new double[] { 0.0, 0.0, Double.MIN_VALUE };
+		controlPanel.setLayout(gbl_controlPanel);
+
+		btnAdd = new JButton("Add");
+		GridBagConstraints gbc_btnAdd = new GridBagConstraints();
+		gbc_btnAdd.insets = new Insets(0, 0, 5, 0);
+		gbc_btnAdd.gridx = 0;
+		gbc_btnAdd.gridy = 0;
+		controlPanel.add(btnAdd, gbc_btnAdd);
+
+		btnTest = new JButton("Test");
+		GridBagConstraints gbc_btnTest = new GridBagConstraints();
+		gbc_btnTest.gridx = 0;
+		gbc_btnTest.gridy = 1;
+		controlPanel.add(btnTest, gbc_btnTest);
 
 		statepane = new JLabel();
-		statepane.setHorizontalAlignment(SwingConstants.LEFT);
+		statepane.setHorizontalAlignment(SwingConstants.CENTER);
+		statepane.setFont(new Font("Tahoma", Font.PLAIN, 13));
 		statepane.setBackground(Color.black);
 		statepane.setForeground(Color.BLACK);
-		frame.getContentPane().add(statepane, BorderLayout.SOUTH);
+		GridBagConstraints gbc_statepane = new GridBagConstraints();
+		gbc_statepane.anchor = GridBagConstraints.SOUTHEAST;
+		gbc_statepane.gridx = 0;
+		gbc_statepane.gridy = 2;
+		frame.getContentPane().add(statepane, gbc_statepane);
 	}
 
+	@SuppressWarnings("deprecation")
 	private void login() {
 		logout();
 		try {
@@ -148,6 +210,6 @@ public class Gui implements ActionListener {
 	}
 
 	public void setState(String state) {
-		statepane.setText("state:  " + state);
+		statepane.setText("state:  " + state + "...");
 	}
 }
