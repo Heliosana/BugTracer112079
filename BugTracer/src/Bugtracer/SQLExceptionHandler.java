6df0eb3d76ce68d29e661d718181d15115199788
Aismael <aismaelinc@web.de>
2013-06-15 13:38:03 +0200

    merge

--- BugTracer/src/Bugtracer/SQLExceptionHandler.java
+++ BugTracer/src/Bugtracer/SQLExceptionHandler.java
@@ -9,19 +9,7 @@ public class SQLExceptionHandler {
 		this.gui = gui;
 
 	}
-<<<<<<< HEAD
 
-	public void SQLExceptionInterpreter(SQLException exe) {
-		gui.setState(exe.getMessage().substring(
-				pointFirstpartendposition(exe.getMessage())));
-		exe.printStackTrace();
-	}
-
-	private int pointFirstpartendposition(String message) {
-
-		return message.indexOf(".") + 1;
-
-=======
 	
 	public void SQLExceptionInterpreter(SQLException exe)
 	{
@@ -35,13 +23,12 @@ public class SQLExceptionHandler {
 			break;
 			
 		default:
-			new ErrorFrame(exe);
+			new ErrorDialog(exe);
 			break;
 		}
 		//gui.setState(exe.getMessage().substring(pointFirstpartendposition(exe.getMessage())));
 		
 		//exe.printStackTrace();
->>>>>>> better exception handler
 	}
 	
 //private int pointFirstpartendposition(String message) {
